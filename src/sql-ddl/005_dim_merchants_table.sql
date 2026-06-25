7606056284b177052cb7d730e98a8065ceabcaa7
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2026-06-25 23:38:32 +0200

    added unique keys for ON CONFLICT error resolution

--- src/sql-ddl/005_dim_merchants_table.sql
+++ src/sql-ddl/005_dim_merchants_table.sql
@@ -1,7 +1,7 @@
 -- Merchant Dimension Table
 CREATE TABLE IF NOT EXISTS dim_merchants (
   id SERIAL PRIMARY KEY,
-  merchant_name VARCHAR(100) NOT NULL, -- e.g., 'Amazon', 'Walmart'
+  merchant_name VARCHAR(100) UNIQUE NOT NULL, -- e.g., 'Amazon', 'Walmart'
   merchant_category VARCHAR(50), -- e.g., 'retail', 'online'
   created_at TIMESTAMPTZ DEFAULT now()
 );

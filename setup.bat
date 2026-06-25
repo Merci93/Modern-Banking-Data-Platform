68628c68577ea00ab298c5f2b4d3d16d13e75317
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2026-06-25 23:20:32 +0200

    setup - added consumer image build script

--- setup.bat
+++ setup.bat
@@ -4,6 +4,7 @@ ECHO Checking existing Docker images ...
 :: Set image names
 SET AIRFLOW_IMAGE=banking-airflow:custom
 SET DEBEZIUM_INIT=debezium-init:custom
+SET CDC_CONSUMER=cdc-consumer:custom
 
 :: Check if AIRFLOW_IMAGE already exists
 SET BUILD_AIRFLOW_IMAGE=true
@@ -23,6 +24,15 @@ FOR /F "tokens=*" %%i IN ('docker images -q %DEBEZIUM_INIT%') DO (
     )
 )
 
+:: Check if CDC_COMSUMER image already exists
+SET BUILD_CONSUMER=true
+FOR /F "tokens=*" %%i IN ('docker images -q %CDC_CONSUMER%') DO (
+    IF NOT "%%i"=="" (
+        ECHO [SKIP] %CDC_CONSUMER% already exists. Skipping build.
+        SET BUILD_CONSUMER=false
+    )
+)
+
 IF "%BUILD_AIRFLOW_IMAGE%"=="true" (
     ECHO [BUILD] Building %AIRFLOW_IMAGE% image ...
     docker build -t %AIRFLOW_IMAGE% -f dockerfile-airflow.dockerfile .
@@ -33,6 +43,12 @@ IF "%BUILD_DEBEZIUM_INIT%"=="true" (
     ECHO [BUILD] Building %DEBEZIUM_INIT% image ...
     docker build -t %DEBEZIUM_INIT% -f dockerfile-debezium-init.dockerfile .
     ECHO [INFO] %DEBEZIUM_INIT% image build completed.
+)
+
+IF "%BUILD_CONSUMER%"=="true" (
+    ECHO [BUILD] Building %CDC_CONSUMER% image ...
+    docker build -t %CDC_CONSUMER% -f dockerfile-cdc-consumer.dockerfile .
+    ECHO [INFO] %CDC_CONSUMER% image build completed.
 )
 
 ECHO [UP] Starting containers ...

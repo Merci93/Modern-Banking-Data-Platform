4521450c9b2cb5b9b80cfe2af8b33003f0ed98f7
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2026-06-30 14:19:10 +0200

    updated column variable limits

--- data-generator/sql-ddl/002_dim_customers_table.sql
+++ data-generator/sql-ddl/002_dim_customers_table.sql
@@ -1,12 +1,12 @@
 -- Customer Dimension Table
 CREATE TABLE IF NOT EXISTS dim_customers (
   id SERIAL PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  middle_name VARCHAR(50),
-  last_name VARCHAR(50) NOT NULL,
+  first_name VARCHAR(100) NOT NULL,
+  middle_name VARCHAR(100),
+  last_name VARCHAR(100) NOT NULL,
   phone VARCHAR(20) UNIQUE NOT NULL,
-  email VARCHAR(50) UNIQUE NOT NULL,
-  country VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  country VARCHAR(100) NOT NULL,
   address VARCHAR(100) NOT NULL,
   created_at TIMESTAMPTZ DEFAULT now(),
   updated_at TIMESTAMPTZ

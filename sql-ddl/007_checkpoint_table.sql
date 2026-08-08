57c6a7f53b5276001089f7d6e0126759704fdf7d
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2026-08-08 19:57:27 +0200

    MinIO checkpoint data table

--- sql-ddl/007_checkpoint_table.sql
+++ sql-ddl/007_checkpoint_table.sql
@@ -1,6 +1,6 @@
 -- Checkpointing table for data transfer from MinIO to Databricks
 CREATE TABLE ingestion_checkpoint (
-    table_name      VARCHAR(100) PRIMARY KEY,
-    object_key      TEXT NOT NULL,
-    updated_at      TIMESTAMP DEFAULT NOW()
+    table_name  VARCHAR(100) PRIMARY KEY,
+    object_key  TEXT         NOT NULL,
+    updated_at  TIMESTAMP    DEFAULT NOW()
 );

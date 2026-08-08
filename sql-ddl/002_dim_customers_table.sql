150846b8f53492dad847fb6c7f2804656ea03b6e
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2026-08-08 19:32:21 +0200

    customers table DDL

--- sql-ddl/002_dim_customers_table.sql
+++ sql-ddl/002_dim_customers_table.sql
@@ -1,13 +1,13 @@
 -- Customer Dimension Table
 CREATE TABLE IF NOT EXISTS dim_customers (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(100) NOT NULL,
-  middle_name VARCHAR(100),
-  last_name VARCHAR(100) NOT NULL,
-  phone VARCHAR(20) UNIQUE NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  country VARCHAR(100) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  created_at TIMESTAMPTZ DEFAULT now(),
-  updated_at TIMESTAMPTZ
+  id         SERIAL PRIMARY KEY,
+  firstName  VARCHAR(100) NOT NULL,
+  middleName VARCHAR(100),
+  lastName   VARCHAR(100) NOT NULL,
+  phone      VARCHAR(20)  UNIQUE NOT NULL,
+  email      VARCHAR(100) UNIQUE NOT NULL,
+  country    VARCHAR(100) NOT NULL,
+  address    VARCHAR(100) NOT NULL,
+  createdAt  TIMESTAMPTZ DEFAULT now(),
+  updatedAt  TIMESTAMPTZ
 ); 
